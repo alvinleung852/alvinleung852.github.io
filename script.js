b2190208162a6f769c8a6e7cae4db685efc88afc
alvinleung <alvin.ho.leung@gmail.com>
2014-07-18 15:26:06 -0400

    fixed menu bar

--- script.js
+++ script.js
@@ -17,57 +17,51 @@ $(document).ready(function() {
             height: '100%',
             }, 350, function() {
         }); 
-        $('.nav_wrapper').show();
-        // $('.name').css('top', 'auto');  
-        // $('.name').animate({
-        // 	bottom: '10px',        	
-        // 	}, 350, function() {
-        // });        
+        $('.nav_wrapper').fadeIn(350);    
         $('.wrapper').slideToggle(400);   
         lastContent = temp[0];
     });
 
 	//close content function
 	$('.close_content').click(function() {	
-    $('.' + lastContent).find('h2').replaceWith(function(){
+        $('.' + lastContent).find('h2').replaceWith(function(){
             return '<h1>' + $(this).text() +'</h1>';
         });	
         $('.content').animate({
             height: '0%',
             }, 350, function() {
         });
-        // $('.name').css('bottom', 'auto'); 
-        // $('.name').css('top', '-50px'); 
-        // $('.name').animate({
-        // 	top: '10px',        	
-        // 	}, 350, function() {
-        // });   
-    $('.nav_wrapper').hide();
+        $('.nav_wrapper').fadeOut(350);
         $('.content').hide(350);  
         $('.wrapper').slideToggle(300);
     });
 
 	//navigate across section function
-    $('.cross_link').click(function() {	
-        $('.' + lastContent).find('h2').replaceWith(function(){
-            return '<h1>' + $(this).text() +'</h1>';
-        });
-        $('.' + lastContent + '_content').fadeOut(350);    
-    	$('.' + lastContent + '_content').animate({
-            height: '0%',
-            }, 350, function() {
-        });
-        $('.' + lastContent + '_content').hide(350); 	
+    $('.cross_link').click(function() {
         temp = this.classList;
-        $('.' + temp[0]).find('h1').replaceWith(function(){
-            return '<h2>' + $(this).text() +'</h2>';
-        });
-		$('.' + temp[0] + '_content').show();
-		$('.' + temp[0] + '_content').animate({
-            height: '100%',
-            }, 350, function() {
-        });
-        lastContent = temp[0];
+    	if(temp[0] != lastContent){
+            $('.' + lastContent).find('h2').replaceWith(function(){
+                return '<h1>' + $(this).text() +'</h1>';
+            });
+            $('.' + lastContent).toggleClass('cross_link');
+
+            $('.' + lastContent + '_content').fadeOut(350);    
+        	$('.' + lastContent + '_content').animate({
+                height: '0%',
+                }, 350, function() {
+            });
+        
+            $('.' + temp[0]).find('h1').replaceWith(function(){
+                return '<h2>' + $(this).text() +'</h2>';
+            });
+            $('.' + temp[0] + '_content').show();
+            $('.' + temp[0] + '_content').animate({
+                height: '100%',
+                }, 350, function() {
+            });
+            lastContent = temp[0];
+        }
+        
     });
 });
 
